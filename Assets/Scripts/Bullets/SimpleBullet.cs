dfc11232f18b44061882a69d39ffd9bb1774c950
Polakowicz <PolakowiczYT@gmail.com>
2022-03-15 22:05:25 +0100

    update enemy hp after hit

--- Assets/Scripts/Bullets/SimpleBullet.cs
+++ Assets/Scripts/Bullets/SimpleBullet.cs
@@ -7,10 +7,19 @@ public class SimpleBullet : MonoBehaviour
 
     //Parameters
     [SerializeField] float speed = 10f;
+    [SerializeField] int damage = 5;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2 (-Mathf.Sin(Mathf.Deg2Rad * rb.rotation), Mathf.Cos(Mathf.Deg2Rad * rb.rotation)) * speed;
     }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.gameObject.layer != 16) return; //16 = enemy
+
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        enemy.OnGetHit(damage);
+	}
 }
