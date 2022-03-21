dccb7033df57fa31237147a1751a3e9273af03cc
Michał Polak <88286798+Polakowicz@users.noreply.github.com>
2022-03-21 20:09:10 +0100

    Refactor player controlls script (#12)
    * fix rotation when moving
    
    * clean up player aim script
    
    * add melee range collider
    
    * remove required component line from player movement script
    
    * fix typo
    
    * make wpons and player controlls struckture
    
    * add weapon switching
    
    * add controlls for gamepad
    
    * change input positions in window
    
    * fix controller input
    
    * add prefab to level
    
    * add shooting
    
    * update level scene
    
    * add missing mele basic attack
    
    * update enemy on scene

---
+++ Assets/Scripts/Player/Weapon/RangedWeapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[Serializable]
+public class RangedWeapon : Weapon
+{
+	//Components
+	[SerializeField] Transform gunTransform;
+	[SerializeField] GameObject bulletPrefab;
+
+	public override void PerformeBasicAttack()
+	{
+		UnityEngine.Object.Instantiate(bulletPrefab, gunTransform.position, gunTransform.rotation);
+	}
+}
