893c3efa45f17b8f86dc5d1a81c55212a08cf92a
Michał Polak <88286798+Polakowicz@users.noreply.github.com>
2022-03-24 00:07:05 +0100

    Blaster auto fire (#16)
    * add autofire
    
    * delete unnecessary testing scene
    
    * add decreasing accuracy to autofire
    
    * update varibles names
    
    * save parameters variables to prefab
    
    * change for percentage firerate and dispersion increase

--- Assets/Scripts/Player/Weapon/RangedWeapon.cs
+++ Assets/Scripts/Player/Weapon/RangedWeapon.cs
@@ -8,6 +8,21 @@ public class RangedWeapon : Weapon
 	//Components
 	[SerializeField] Transform gunTransform;
 	[SerializeField] GameObject bulletPrefab;
+	[SerializeField] MonoBehaviour player;
+
+	//Parameters
+	[SerializeField] float startFireRate;
+	[SerializeField] float maxFireRate;
+	[SerializeField] float fireRatePercentageIncrease;
+
+	[SerializeField] float startDispersion;
+	[SerializeField] float maxDispersion;
+	[SerializeField] float dispersinPercentageIncrease;
+
+	//Internal variables
+	Coroutine autoFireCoroutine;
+	float fireRate;
+	float dispersion;
 
 	public override void PerformBasicAttack()
 	{
@@ -16,15 +31,38 @@ public class RangedWeapon : Weapon
 
 	public override void PerformStrongerAttack() 
 	{
-		Debug.Log("Range stronger attack start");
+		autoFireCoroutine = player.StartCoroutine(AutoFire());
 	}
 	public override void CancelStrongerAttack()
 	{
-		Debug.Log("Range stronger attack end");
+		player.StopCoroutine(autoFireCoroutine);
 	}
 
 	public override void PerformAlternativeAttack()
 	{
 		Debug.Log("Range aalternative attack");
+	}
+
+	IEnumerator AutoFire()
+	{
+		fireRate = startFireRate;
+		dispersion = startDispersion;
+		while (true) {
+			UnityEngine.Object.Instantiate(bulletPrefab, gunTransform.position, GetRandomisedAccuracy());
+			yield return new WaitForSeconds(fireRate);
+			if (dispersion < maxDispersion) {
+				dispersion += dispersinPercentageIncrease * Mathf.Abs(maxDispersion - dispersion);
+			}
+			if (fireRate > maxFireRate) {
+				fireRate -= fireRatePercentageIncrease * Mathf.Abs(maxFireRate - fireRate);
+			}
+		}
+	}
+
+	Quaternion GetRandomisedAccuracy()
+	{
+		var rotation = gunTransform.rotation.eulerAngles.z;
+		float newRotation = UnityEngine.Random.Range(rotation - dispersion, rotation + dispersion);
+		return Quaternion.Euler(0,0,newRotation);
 	}
 }
