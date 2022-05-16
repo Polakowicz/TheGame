5395f28ef1522070d70615e1bdc94beff25cce51
Polakowicz <PolakowiczYT@gmail.com>
2022-05-16 17:04:56 +0200

    add pushable rock script

---
+++ Assets/Scripts/Enemy/Boss/PushableRock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PushableRock : MonoBehaviour
+{
+	private Rigidbody2D rb;
+
+	[SerializeField] private float speed;
+
+	bool pushed;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	public void Push(Vector2 direction)
+	{
+		pushed = true;
+		rb.velocity = direction.normalized * speed;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (!pushed) return;
+
+		if(collision.gameObject.layer == LayerMask.NameToLayer("Boss")) {
+			Debug.Log("Damage boss with rock");
+		}
+
+		Destroy(gameObject);
+	}
+}
