04b881d9c9c2da4e9f2c676f338067c15c3bb1c1
Polakowicz <PolakowiczYT@gmail.com>
2022-06-21 17:31:04 +0200

    add-boss-2-basic-script

---
+++ Assets/Scripts/Enemy/Boss/Smoker/Smoker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Smoker : MonoBehaviour, IPullable
+{
+	[SerializeField] GameObject bullet;
+
+	private Animator animator;
+	public GameObject Player {get; private set;}
+
+	private int pullsToDie = 3;
+
+	private bool alive;
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+		Player = GameObject.FindGameObjectWithTag("Player");
+		alive = true;
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	public void Pull(float speed)
+	{
+		pullsToDie--;
+		if(pullsToDie <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
