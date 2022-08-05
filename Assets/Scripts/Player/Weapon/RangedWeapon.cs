d59152bf716682465fd7cac6497d26db1f32c8d3
Michał Polak <mi.polak@outlook.com>
2022-08-05 21:06:05 +0200

    clean range weapon skript

--- Assets/Scripts/Player/Weapon/RangedWeapon.cs
+++ Assets/Scripts/Player/Weapon/RangedWeapon.cs
@@ -7,69 +7,68 @@ namespace script.Player.Weapon
 	[Serializable]
 	public class RangedWeapon : Weapon
 	{
-		private PlayerManager player;
-
-		private Coroutine autoFireCoroutine;
-
 		//Components
-		[SerializeField] Transform gunTransform;
-		[SerializeField] GameObject defaultBullet;
-		[SerializeField] GameObject explosiveBullet;
-		[SerializeField] GameObject piercingBullet;
-		LineRenderer lineRenderer;
-
-		//Parameters
-		[SerializeField] float startFireRate;
-		[SerializeField] float maxFireRate;
-		[SerializeField] float fireRatePercentageIncrease;
-
-		[SerializeField] float startDispersion;
-		[SerializeField] float maxDispersion;
-		[SerializeField] float dispersinPercentageIncrease;
-
-		[SerializeField] LayerMask beamHitLayerMask;
-		[SerializeField] float beamPullSpeed;
-		[SerializeField] float beamDistance = 10f;
-		[SerializeField] float beamPullCooldown;
-		[SerializeField] float beamStunTime = 1;
-
-		//Internal variables
-		GameObject beamHit;
+		private PlayerManager player;
 		
-		GameObject bullet;
-		float fireRate;
-		float dispersion;
 
-		void Start()
+		[SerializeField] private Transform gunBarrel;
+		private GameObject bullet;
+
+		//Bullet Types
+		[SerializeField] private GameObject defaultBullet;
+		[SerializeField] private GameObject explosiveBullet;
+		[SerializeField] private GameObject piercingBullet;
+
+		//AutoFire
+		[SerializeField] private float autoFireStartRate;
+		[SerializeField] private float autoFireMaxRate;
+		[SerializeField] private float autoFireRateIncrease;
+		[SerializeField] private float autoFireStartDispersion;
+		[SerializeField] private float autoFireMaxDispersion;
+		[SerializeField] private float autoFireDispersinIncrease;
+		private Coroutine autoFireCoroutine;
+		private float autoFireRate;
+		private float autoFireDispersion;
+
+		//Beam
+		[SerializeField] private LayerMask beamLayerMask;
+		[SerializeField] private float beamPullSpeed;
+		[SerializeField] private float beamDistance = 10f;
+		[SerializeField] private float beamPullCooldown;
+		[SerializeField] private float beamStunTime = 1;
+		private LineRenderer bimRenderer;
+		private GameObject beamHit;
+
+		private void Start()
 		{
 			player = GetComponentInParent<PlayerManager>();
+			bimRenderer = GetComponent<LineRenderer>();
 
 			Type = WeaponType.Blaster;
-
-			lineRenderer = GetComponent<LineRenderer>();
-			lineRenderer.enabled = false;
 			bullet = defaultBullet;
 
+			bimRenderer.enabled = false;
+	
 			player.powerUpController.OnPowerUpChanged += ChangePowerUp;
 		}
 
 		void Update()
 		{
 			if (beamHit != null) {
-				lineRenderer.SetPosition(0, transform.position);
-				lineRenderer.SetPosition(1, beamHit.transform.position);
+				bimRenderer.SetPosition(0, transform.position);
+				bimRenderer.SetPosition(1, beamHit.transform.position);
 			}
 		}
 
 		//Shooting
 		private void ShootBullet()
 		{
-			ShootBullet(gunTransform.rotation);
+			ShootBullet(gunBarrel.rotation);
 		}
 		private void ShootBullet(Quaternion rotation)
 		{
 			player.AudioManager.Play("RangedBasicAttack");
-			Instantiate(bullet, gunTransform.position, rotation);
+			Instantiate(bullet, gunBarrel.position, rotation);
 
 			if (bullet == explosiveBullet) {
 				player.powerUpController.ShootExplosiveBullet();
@@ -92,23 +91,23 @@ namespace script.Player.Weapon
 		}
 		IEnumerator AutoFire()
 		{
-			fireRate = startFireRate;
-			dispersion = startDispersion;
+			autoFireRate = autoFireStartRate;
+			autoFireDispersion = autoFireStartDispersion;
 			while (true) {
 				ShootBullet(GetRandomisedAccuracy());
-				yield return new WaitForSeconds(fireRate);
-				if (dispersion < maxDispersion) {
-					dispersion += dispersinPercentageIncrease * Mathf.Abs(maxDispersion - dispersion);
+				yield return new WaitForSeconds(autoFireRate);
+				if (autoFireDispersion < autoFireMaxDispersion) {
+					autoFireDispersion += autoFireDispersinIncrease * Mathf.Abs(autoFireMaxDispersion - autoFireDispersion);
 				}
-				if (fireRate > maxFireRate) {
-					fireRate -= fireRatePercentageIncrease * Mathf.Abs(maxFireRate - fireRate);
+				if (autoFireRate > autoFireMaxRate) {
+					autoFireRate -= autoFireRateIncrease * Mathf.Abs(autoFireMaxRate - autoFireRate);
 				}
 			}
 		}
 		Quaternion GetRandomisedAccuracy()
 		{
-			var rotation = gunTransform.rotation.eulerAngles.z;
-			float newRotation = UnityEngine.Random.Range(rotation - dispersion, rotation + dispersion);
+			var rotation = gunBarrel.rotation.eulerAngles.z;
+			float newRotation = UnityEngine.Random.Range(rotation - autoFireDispersion, rotation + autoFireDispersion);
 			return Quaternion.Euler(0, 0, newRotation);
 		}
 
@@ -124,18 +123,18 @@ namespace script.Player.Weapon
 		//Beam
 		public override void StartAlternativeAttack()
 		{
-			var hit = Physics2D.Raycast(gunTransform.position, gunTransform.up, beamDistance, beamHitLayerMask);
+			var hit = Physics2D.Raycast(gunBarrel.position, gunBarrel.up, beamDistance, beamLayerMask);
 			if (hit.collider != null) {
 				beamHit = hit.collider.gameObject;
-				lineRenderer.SetPosition(0, transform.position);
-				lineRenderer.SetPosition(1, beamHit.transform.position);
-				lineRenderer.enabled = true;
+				bimRenderer.SetPosition(0, transform.position);
+				bimRenderer.SetPosition(1, beamHit.transform.position);
+				bimRenderer.enabled = true;
 			}
 		}
 		public override void CancelAlternativeAttack()
 		{
 			beamHit = null;
-			lineRenderer.enabled = false;
+			bimRenderer.enabled = false;
 		}
 
 		//Pull
