84e18c586acb58b40c282b00f1cde9670e24b133
Michał Polak <mi.polak@outlook.com>
2022-08-05 20:43:46 +0200

    clean player aim script

--- Assets/Scripts/Player/PlayerAim.cs
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -1,50 +1,58 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerAim : MonoBehaviour
+namespace scritp.Player
 {
-	//Components
-	[SerializeField] PlayerManager eventSystem;
-	[SerializeField] PlayerInput playerInput;
-	[SerializeField] Transform crosshair;
-
-	//Input actions
-	InputAction aimAction;
-
-	//Internal variables
-	bool gamepad;
-	float gamepadCrosshariDistance;
-
-	void Start()
+	public class PlayerAim : MonoBehaviour
 	{
-		aimAction = playerInput.actions["Aim"];
-		gamepadCrosshariDistance = (crosshair.position - transform.position).magnitude;
-	}
+		//Components
+		private PlayerManager player;
+		private PlayerInput input;
 
-	void Update()
-	{
-		Vector2 lookDirection;
-		if (gamepad) {
-			lookDirection = aimAction.ReadValue<Vector2>();
-			if (lookDirection == Vector2.zero) {
-				return;
-			}
-			crosshair.transform.localPosition = Vector2.up * gamepadCrosshariDistance;
-		} else {
-			var mousePos = (Vector2)Camera.main.ScreenToWorldPoint(aimAction.ReadValue<Vector2>());
-			crosshair.transform.position = mousePos;
-			lookDirection = mousePos - (Vector2)transform.position;
+		//Parameters
+		[SerializeField] private Transform crosshair;
+
+		//Input actions
+		private InputAction aimAction;
+
+		//Internal variables
+		private bool gamepad;
+		private float gamepadCrosshariDistance;
+
+		private void Start()
+		{
+			player = GetComponentInParent<PlayerManager>();
+			input = GetComponentInParent<PlayerInput>();
+
+			aimAction = input.actions["Aim"];
+			gamepadCrosshariDistance = (crosshair.position - transform.position).magnitude;
 		}
-		transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90)); ;
-		eventSystem.playerData.aimDirection = lookDirection.normalized;
-	}
 
-	public void OnControlsChanged(PlayerInput input)
-	{
-		//To change in future. It is public and made through the inspector because onControlsChange does not work
-		gamepad = input.currentControlScheme.Equals("Gamepad");
-		if (gamepad) {
-			crosshair.localPosition = (Vector2)crosshair.localPosition.normalized * gamepadCrosshariDistance;
+		private void Update()
+		{
+			Vector2 lookDirection;
+			if (gamepad) {
+				lookDirection = aimAction.ReadValue<Vector2>();
+				if (lookDirection == Vector2.zero) {
+					return;
+				}
+				crosshair.transform.localPosition = Vector2.up * gamepadCrosshariDistance;
+			} else {
+				var mousePos = (Vector2)Camera.main.ScreenToWorldPoint(aimAction.ReadValue<Vector2>());
+				crosshair.transform.position = mousePos;
+				lookDirection = mousePos - (Vector2)transform.position;
+			}
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90)); ;
+			player.playerData.aimDirection = lookDirection.normalized;
+		}
+
+		public void OnControlsChanged(PlayerInput input)
+		{
+			//To change in future. It is public and made through the inspector because onControlsChange does not work
+			gamepad = input.currentControlScheme.Equals("Gamepad");
+			if (gamepad) {
+				crosshair.localPosition = (Vector2)crosshair.localPosition.normalized * gamepadCrosshariDistance;
+			}
 		}
 	}
 }
