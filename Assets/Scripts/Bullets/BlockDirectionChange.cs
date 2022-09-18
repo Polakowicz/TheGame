5153f1eb4f3d79a1a48f64addda5efc2941a4e29
Michał Polak <mi.polak@outlook.com>
2022-09-18 15:34:55 +0200

    fix bullet riposet

--- Assets/Scripts/Bullets/BlockDirectionChange.cs
+++ Assets/Scripts/Bullets/BlockDirectionChange.cs
@@ -11,14 +11,12 @@ namespace Scripts.Bullets
 
 		public void Riposte(GameObject sender)
 		{
-			Debug.Log("Bullet riposetd");
-			//gameObject.transform.Rotate(0, 0, 180);
 			if (rb == null) {
 				rb = GetComponent<Rigidbody2D>();
 			}
-			//rb.velocity = new Vector2(-rb.velocity.x, -rb.velocity.y);
-			rb.velocity = rb.position - (Vector2)sender.transform.position;
-			Debug.Log(rb.velocity);
+			rb.velocity = (rb.position - (Vector2)sender.transform.position).normalized * rb.velocity.magnitude;
+			var newAngle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg - 90;
+			transform.rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
 		}
 	}
 }
