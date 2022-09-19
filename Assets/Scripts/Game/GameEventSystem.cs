36013124885d17adbee68f886ed31b4f5c162f94
Michał Polak <88286798+Polakowicz@users.noreply.github.com>
2022-09-19 19:14:12 +0200

    create checkpint script

---
+++ Assets/Scripts/Game/GameEventSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Scripts.Game
+{
+	public class GameEventSystem : MonoBehaviour
+	{
+		public static GameEventSystem Instance;
+
+		private void Awake()
+		{
+			if (Instance == null)
+				Instance = this;
+			else
+				Destroy(gameObject);
+		}
+
+		public Action OnPlayerDied;
+		public Action<int> OnPlayerHPChanged;
+
+		public Action<Checkpoint> OnCheckpointReached;
+	}
+}
