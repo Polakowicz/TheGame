28a55eb0e90fdc5008493056ccc64c5637f01d8a
Michał Polak <88286798+Polakowicz@users.noreply.github.com>
2022-09-25 17:48:55 +0200

    add one wave spawning script

---
+++ Assets/Scripts/Enemies/BossSpawningEnemies.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Scripts.Enemies
+{
+	public class BossSpawningEnemies : MonoBehaviour
+	{
+		[Serializable]
+		public class Spawn
+		{
+			public GameObject emenyPrefab;
+			public Transform position;
+		}
+
+		// List of all enemies to spawn in wave
+		[SerializeField] private Spawn[] enemiesInWave;
+
+		public void SpawnWave()
+		{
+			foreach(var spawn in enemiesInWave)
+			{
+				Instantiate(spawn.emenyPrefab, spawn.position);
+			}
+		}
+	}
+}
