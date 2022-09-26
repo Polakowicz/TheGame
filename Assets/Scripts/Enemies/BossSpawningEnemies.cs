cdec63879e159b4542012a13e224b8df6f3424a3
Michał Polak <88286798+Polakowicz@users.noreply.github.com>
2022-09-26 21:09:31 +0200

    Boss spawning enemy waves (#55)
    * add one wave spawning script
    
    * make spawn component for multiple waves

---
+++ Assets/Scripts/Enemies/BossSpawningEnemies.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Scripts.Enemies
+{
+	public class BossSpawningEnemies : MonoBehaviour
+	{
+		// Class nesting to display 2d array in Unity inspector
+
+		// Class represents single wave of enemies
+		[Serializable]
+		private class Wave
+		{
+			// All enemies in single wave
+			[SerializeField] public EnemySpawn[] enemiesInWave;
+
+			[Serializable]
+			public class EnemySpawn
+			{
+				public GameObject emenyPrefab;
+				public Transform position;
+			}
+		}
+
+		// List of waves
+		[SerializeField] private Wave[] allEnemyWaves;
+		private int waveNumber = 0;
+
+		public void SpawnNextWave()
+		{
+			// Make sure that not out of waves range
+			Assert.IsTrue(waveNumber < allEnemyWaves.Length);
+
+			// Spawn enemies
+			var wave = allEnemyWaves[waveNumber++];
+			foreach(var enemy in wave.enemiesInWave)
+			{
+				Instantiate(enemy.emenyPrefab, enemy.position);
+			}
+		}
+	}
+}
