1a0d3c5f7356f7d292054f6bf6a2bf6b5feae73e
Michał Polak <88286798+Polakowicz@users.noreply.github.com>
2022-10-09 19:06:51 +0200

    refactor bullet scripts

---
+++ Assets/Scripts/Bullets/ExplosiveBullet.cs
@@ -0,0 +1,36 @@
+﻿using Scripts.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Bullets
+{
+	public class ExplosiveBullet : Bullet
+	{
+		// To find enemies to hit
+		[SerializeField] private CircleCollider2D rangeCollider;
+		private ContactFilter2D filter;
+
+		private void Awake()
+		{
+			filter = new ContactFilter2D {
+				layerMask = hitLayerMask,
+				useLayerMask = true,
+				useTriggers = true
+			};
+		}
+
+		protected override void Damage(Collider2D collision)
+		{
+			List<Collider2D> hits = new List<Collider2D>();
+			rangeCollider.OverlapCollider(filter, hits);
+			foreach (Collider2D hit in hits)
+			{
+				var distance = Vector2.Distance(transform.position, hit.transform.position);
+				var dmg = (rangeCollider.radius - distance) / rangeCollider.radius * damage;
+				hit.GetComponent<IHit>().Hit(gameObject, Mathf.FloorToInt(dmg));
+			}
+			bulletPool.DisposeObject(gameObject);
+		}
+	}
+}
