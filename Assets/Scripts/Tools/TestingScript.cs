9eae153cd771b7dbad3a9476561a074d6a8a2d13
Michał Polak <PolakowiczYT@gmail.com>
2022-11-16 00:17:30 +0100

    add testing script

---
+++ Assets/Scripts/Tools/TestingScript.cs
@@ -0,0 +1,14 @@
+﻿using Scripts.Game;
+using System.Collections;
+using UnityEngine;
+
+namespace Scripts.Tools
+{
+    public class TestingScript : MonoBehaviour
+    {
+        private void Start()
+        {
+            GameEventSystem.Instance.OnAllPartsCollected += () => { Debug.Log("All parts collected"); }
+        }
+    }
+}
