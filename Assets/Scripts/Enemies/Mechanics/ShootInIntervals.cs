036ad4d5a47af8374ea3570e4784b3bbe8dd844c
Michał Polak <PolakowiczYT@gmail.com>
2022-11-16 21:09:30 +0100

    add shooting in intervals script

---
+++ Assets/Scripts/Enemies/Mechanics/ShootInIntervals.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemies.Mechanics
+{
+    public class ShootInIntervals : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private Vector2 spawnPosition;
+        [SerializeField] private float interval;
+        [SerializeField] private float bulletSpeed;
+
+        [SerializeField] private bool active;
+        public bool Active
+        {
+            get => active;
+            set
+            {
+                active = value;
+                if (active)
+                {
+                    StartCoroutine(Shoot());
+                }
+            }
+        }
+
+        private IEnumerator Shoot()
+        {
+            while (active)
+            {
+                yield return new WaitForSeconds(interval);
+                var bullet = Instantiate(prefab, spawnPosition, transform.rotation);
+                bullet.GetComponent<Rigidbody2D>().velocity = transform.forward * bulletSpeed;
+            }
+            
+
+
+        }
+        
+    }
+}
