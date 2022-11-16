ebeb04cd7c675303dc957794bc9b05e5c79b494f
Michał Polak <PolakowiczYT@gmail.com>
2022-11-16 21:45:14 +0100

    add active to inspector and fix angle

--- Assets/Scripts/Enemies/Mechanics/RotateTowards.cs
+++ Assets/Scripts/Enemies/Mechanics/RotateTowards.cs
@@ -6,13 +6,14 @@ namespace Scripts.Enemies
     public class RotateTowards : MonoBehaviour
     {
         [SerializeField] private GameObject Target;
-        public bool Active { get; set; }
+        [SerializeField] private bool active;
+        public bool Active { get => active; set => active = value; }
 
         
         void Update()
         {
             var direction = Target.transform.position - transform.position;
-            float angle = Mathf.Atan2(direction.y, direction.x);
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
     }
