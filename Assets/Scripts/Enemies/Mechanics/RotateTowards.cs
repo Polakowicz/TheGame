05776405507f79ed08e2dbcee3db843700f56968
Michał Polak <88286798+Polakowicz@users.noreply.github.com>
2022-11-16 21:56:54 +0100

    Cannon scripts (#64)
    * add roatate toward target script
    
    * fix null error in part
    
    * add shooting in intervals script
    
    * change vector2 to transform
    
    * change for object pool
    
    * add active to inspector and fix angle
    
    * fix namespace
    
    * delete unnecessary variable
    
    * Fix shoot coroutine
    
    * fix formatting
    
    * add cannon bullet pool to scene

---
+++ Assets/Scripts/Enemies/Mechanics/RotateTowards.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Scripts.Enemies
+{
+    public class RotateTowards : MonoBehaviour
+    {
+        [SerializeField] private GameObject Target;
+        [SerializeField] private bool active;
+        public bool Active { get => active; set => active = value; }
+
+        
+        void Update()
+        {
+            var direction = Target.transform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
