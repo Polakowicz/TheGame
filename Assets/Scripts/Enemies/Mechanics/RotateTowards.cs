ce31ebeb6ef1c4dda2f55e22deb58684013dcd44
Michał Polak <PolakowiczYT@gmail.com>
2022-11-16 20:24:34 +0100

    add roatate toward target script

---
+++ Assets/Scripts/Enemies/Mechanics/RotateTowards.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Scripts.Enemies
+{
+    public class RotateTowards : MonoBehaviour
+    {
+        [SerializeField] private GameObject Target;
+        public bool Active { get; set; }
+
+        
+        void Update()
+        {
+            var direction = Target.transform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x);
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
