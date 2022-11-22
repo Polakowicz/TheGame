cd0608f33a123c732975d0c50eef426d1e8b7fda
Michał Polak <PolakowiczYT@gmail.com>
2022-11-22 23:56:46 +0100

    add item droping when dying

--- Assets/Scripts/Enemies/Mechanics/EnemyDieAndDropBoosters.cs
+++ Assets/Scripts/Enemies/Mechanics/EnemyDieAndDropBoosters.cs
@@ -5,6 +5,19 @@ namespace Scripts.Enemies
 {
     public class EnemyDieAndDropBoosters : MonoBehaviour
     {
+        [SerializeField] private GameObject[] itemsList;
+        [SerializeField] private float chanceToDropItem = 0.15f;
+
+        public void DropRandomItem()
+        {
+            var random = Random.Range(0f, 1f);
+            if (random > chanceToDropItem) return;
+
+            random = Random.Range(0, itemsList.Length);
+            var position = (Vector2)transform.position + Random.insideUnitCircle;
+            Instantiate(itemsList[Mathf.FloorToInt(random)], position, Quaternion.identity);
+        }
+
         public void Die()
         {
             Destroy(gameObject);
